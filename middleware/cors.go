d9a1ebfa52f61cd27a3e4ec25a52581a2b19aa4f
Bruno Luiz Silva <contact@brunoluiz.net>
2019-12-12 23:15:19 +0000

    change cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,7 +12,7 @@ func Cors(next http.Handler) *CorsMiddleware {
 
 func (h *CorsMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	h.next.ServeHTTP(w, r)
 }
