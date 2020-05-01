b1cd71da186c6a704863411b0d9e9653da8fa3d8
Bruno Luiz Silva <contact@brunoluiz.net>
2020-05-01 08:56:57 +0100

    fix: disable NONCE for now

--- middleware/helmet.go
+++ middleware/helmet.go
@@ -9,13 +9,13 @@ import (
 // Helmet add some security defaults for any request
 func Helmet(next http.Handler) http.Handler {
 	helmet := secure.New(secure.Options{
-		STSSeconds:            31536000,
-		STSIncludeSubdomains:  true,
-		STSPreload:            true,
-		FrameDeny:             true,
-		ContentTypeNosniff:    true,
-		BrowserXssFilter:      true,
-		ContentSecurityPolicy: "script-src $NONCE",
+		STSSeconds:           31536000,
+		STSIncludeSubdomains: true,
+		STSPreload:           true,
+		FrameDeny:            true,
+		ContentTypeNosniff:   true,
+		BrowserXssFilter:     true,
+		// ContentSecurityPolicy: "script-src $NONCE",
 	})
 
 	return helmet.Handler(next)
